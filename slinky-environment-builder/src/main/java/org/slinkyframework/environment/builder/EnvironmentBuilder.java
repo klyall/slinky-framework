2ed8ddc299cfe37dcdc718d6dad4286dbd7b2c4a
Keith Lyall <keith.lyall@gmail.com>
2016-08-15 20:11:59 +0100

    Allow EnvironmentBuilders to clean up after themselves.

--- slinky-environment-builder/src/main/java/org/slinkyframework/environment/builder/EnvironmentBuilder.java
+++ slinky-environment-builder/src/main/java/org/slinkyframework/environment/builder/EnvironmentBuilder.java
@@ -7,4 +7,5 @@ import java.util.Set;
 public interface EnvironmentBuilder<T extends BuildDefinition> {
     void setUp(Set<T> buildDefinitions);
     void tearDown(Set<T> buildDefinitions);
+    void cleanUp();
 }
