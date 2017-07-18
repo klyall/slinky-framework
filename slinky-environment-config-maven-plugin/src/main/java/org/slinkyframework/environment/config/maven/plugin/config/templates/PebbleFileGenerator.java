24bd8bfbc86a43795670912f7b31b3d79ea66ea2
Keith Lyall <keith.lyall@gmail.com>
2017-07-18 15:50:07 +0100

    Create a 'nice' error message when property is missing.

--- slinky-environment-config-maven-plugin/src/main/java/org/slinkyframework/environment/config/maven/plugin/config/templates/PebbleFileGenerator.java
+++ slinky-environment-config-maven-plugin/src/main/java/org/slinkyframework/environment/config/maven/plugin/config/templates/PebbleFileGenerator.java
@@ -2,6 +2,7 @@ package org.slinkyframework.environment.config.maven.plugin.config.templates;
 
 import com.mitchellbosecke.pebble.PebbleEngine;
 import com.mitchellbosecke.pebble.error.PebbleException;
+import com.mitchellbosecke.pebble.error.RootAttributeNotFoundException;
 import com.mitchellbosecke.pebble.template.PebbleTemplate;
 import com.typesafe.config.Config;
 import org.apache.commons.io.FileUtils;
@@ -16,6 +17,12 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.nio.charset.Charset;
+import java.util.List;
+
+import static java.lang.String.format;
+import static org.apache.commons.lang3.StringUtils.stripEnd;
+import static org.apache.commons.lang3.StringUtils.stripStart;
 
 public class PebbleFileGenerator implements FileGenerator {
 
@@ -41,7 +48,6 @@ public class PebbleFileGenerator implements FileGenerator {
         Writer writer = null;
 
         try {
-
             File targetSubDir = new File(targetDir, subDir);
             FileUtils.forceMkdir(targetSubDir);
 
@@ -50,13 +56,23 @@ public class PebbleFileGenerator implements FileGenerator {
             String targteFilename = StringUtils.removeEndIgnoreCase(templateFile.getName(), ".tmpl");
             File targetFile = new File(targetSubDir, targteFilename);
 
-            LOG.debug("Generating config file {}", targetFile);
+            LOG.debug("Generating config file '{}' using properties {}", targetFile, config);
 
             writer = new FileWriter(targetFile);
             compiledTemplate.evaluate(writer, new ImmutableConfigMap(config.root()));
 
+        } catch (RootAttributeNotFoundException e) {
+            try {
+                List<String> lines = FileUtils.readLines(templateFile, Charset.defaultCharset());
+                String lineInError = lines.get(e.getLineNumber() - 1);
+                String missingVariable = stripStart(stripEnd(lineInError, "}}"), "{{").trim();
+
+                throw new EnvironmentConfigException(format("The property '%s' is missing from the configuration files", missingVariable));
+            } catch (IOException e1) {
+                throw new EnvironmentConfigException("Error reading template line", e1);
+            }
         } catch (IOException | PebbleException e) {
-            throw new EnvironmentConfigException(String.format("Error processing template %s", templateFile), e);
+            throw new EnvironmentConfigException(format("Error processing template %s", templateFile), e);
         } finally {
             IOUtils.closeQuietly(writer);
         }
